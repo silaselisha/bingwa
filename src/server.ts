b2ad8f989c61683f77c32ac6a5f2dac8e962cacf
Elisha <elisilas87@outlook.com>
2023-11-29 22:17:47 +0300

    project setup fix

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,12 @@ import path from 'path'
 
 import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
 import pino from 'pino'
 
 dotenv.config({ path: path.join(__dirname, '..', '.env') })
 const logger = pino()
 const app = express()
 const port: string = process.env.PORT ?? '8080'
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
 const server = app.listen(() => {
   logger.info(`Listening http://localhost:${port}`)
