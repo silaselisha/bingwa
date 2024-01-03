03c21500c18c130d796d426c269c95de3459f9f9
Elisha <elisilas87@outlook.com>
2024-01-03 16:45:46 +0300

    deactivate &and delete users

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -1,6 +1,8 @@
-import { type resetPasswordParams, type deactivateUserParams, type updateUserParams } from '../controllers/user-controller'
+import { type deactivateUserParams, type updateUserParams } from '../controllers/user-controller'
 import { type IUser, type UserModel } from '../models/user-model'
 import UtilsError from '../utils/app-error'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 class UserServices {
   constructor (private readonly _userModel: UserModel) {}
@@ -10,7 +12,7 @@ class UserServices {
     return users
   }
 
-  getUserById = async (id: string): Promise<any> => {
+  getUserById = async (id: string): Promise<IUser> => {
     const user: IUser = await this._userModel.findById(id).populate({ path: 'password', select: true }) as IUser
     if (user === undefined) {
       throw new UtilsError('could not update users data', 404)
@@ -18,13 +20,33 @@ class UserServices {
     return user
   }
 
-  getUserByIdAndUpdate = async (data: updateUserParams | deactivateUserParams | resetPasswordParams, id: string): Promise<IUser> => {
-    const user = await this._userModel.findByIdAndUpdate(id, data, { new: true, timestamps: false }) as IUser
+  getUserByIdAndUpdate = async (data: updateUserParams | deactivateUserParams, id: string): Promise<IUser> => {
+    const bool = 'isActive' in data
+    const user = await this._userModel.findByIdAndUpdate(id, data, { new: true, timestamps: bool }) as IUser
 
     if (user === undefined) {
       throw new UtilsError('could not update users data', 404)
     }
     return user
+  }
+
+  getInactiveUsers = async (): Promise<IUser[]> => {
+    const users: Array<Promise<IUser>> = await this._userModel.find({ isActive: false }).exec()
+    dayjs.extend(relativeTime)
+
+    const resp = await Promise.all(users)
+    const inactiveUsers = resp.map((user): IUser | undefined => {
+      if (dayjs(user.updatedAt).fromNow(true) === 'a month') {
+        return user
+      }
+      return undefined
+    })
+
+    return inactiveUsers.filter((value) => value !== undefined) as unknown as IUser[]
+  }
+
+  deleteUserById = async (id: string): Promise<void> => {
+    await this._userModel.findByIdAndDelete(id)
   }
 }
 
